e7b6e8d21067a806fa41cd45577fb21e3f58e1ed
alanhou <info@iscbj.com>
2023-08-08 15:31:56 +0800

    运行ARSession

--- ARDemo/ContentView.swift
+++ ARDemo/ContentView.swift
@@ -21,11 +21,13 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        let config = ARWorldTrackingConfiguration()
+        if ARWorldTrackingConfiguration.isSupported {
+            config.planeDetection = .horizontal
+            arView.session.run(config, options: [])
+        }
+        
         
         return arView
         
