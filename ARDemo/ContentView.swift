f8cf00dfe5f261ae2e76928ef59cd500c132585c
alanhou <info@iscbj.com>
2023-08-08 15:21:37 +0800

    feat:initial project

---
+++ ARDemo/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  ARDemo
+//
+//  Created by Alan on 8/8/23.
+//
+
+import SwiftUI
+import RealityKit
+import ARKit
+
+struct ContentView : View {
+    var body: some View {
+        ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
+
