a53127156bca166a7bb13b6e365a9c2045237efa
alanhou <info@iscbj.com>
2023-08-09 17:30:21 +0800

    代码清单3-8:模型动画

--- ARDemo/ContentView.swift
+++ ARDemo/ContentView.swift
@@ -26,7 +26,7 @@ struct ARViewContainer: UIViewRepresentable {
         config.worldAlignment = .gravity
         arView.session.run(config, options: [])
         arView.session.delegate = arView
-        arView.createPlane()
+        arView.createRobot()
         return arView
     }
     
@@ -34,56 +34,19 @@ struct ARViewContainer: UIViewRepresentable {
     
 }
 
-var cubeEntity: ModelEntity?
-var gestureStartLocation: SIMD3<Float>?
-
 extension ARView: ARSessionDelegate{
-    func createPlane() {
+    func createRobot() {
         let planeAnchor = AnchorEntity(plane: .horizontal)
         do {
-            let cubeMesh = MeshResource.generateBox(size: 0.1)
-            var cubeMaterial = SimpleMaterial(color: .white, isMetallic: false)
-            cubeMaterial.color = try .init(texture: .init(.load(named: "Box_Texture")))
-            cubeEntity = ModelEntity(mesh: cubeMesh, materials: [cubeMaterial])
-            cubeEntity?.generateCollisionShapes(recursive: false)
-            cubeEntity?.name = "this is a cube"
-            planeAnchor.addChild(cubeEntity!)
+            let robot = try ModelEntity.load(named: "toy_drummer")
+            planeAnchor.addChild(robot)
+            robot.scale = [0.01, 0.01, 0.01]
             self.scene.addAnchor(planeAnchor)
-            self.installGestures(.all, for: cubeEntity!).forEach{
-                $0.addTarget(self, action: #selector(handleModelGesture))
-            }
+            print("Total animation count: \(robot.availableAnimations.count)")
+            robot.playAnimation(robot.availableAnimations[0].repeat())
         } catch {
-            print("找不到文件")
+            print("找不到USDZ文件")
         }
-    }
-    
-    @objc func handleModelGesture(_ sender: Any) {
-        switch sender {
-        case let rotation as EntityRotationGestureRecognizer:
-            rotation.isEnabled = false
-            var transform = rotation.entity!.transform
-            transform.rotation = simd_quatf(angle: .pi * 1.5, axis: [0, 1, 0])
-            rotation.entity?.move(to: transform, relativeTo: nil, duration: 5.0)
-            rotation.isEnabled = true
-        case let translation as EntityTranslationGestureRecognizer:
-            translation.isEnabled = false
-            var transform = translation.entity!.transform
-            transform.translation = SIMD3<Float>(x: 0.8, y: 0, z: 0)
-            translation.entity?.move(to: transform, relativeTo: nil, duration: 5.0)
-            translation.isEnabled = true
-        case let Scale as EntityScaleGestureRecognizer:
-            Scale.isEnabled = false
-            var scaleTransform = Scale.entity!.transform
-            scaleTransform.scale = SIMD3<Float>(x: 2, y: 2, z: 2)
-            Scale.entity?.move(to: scaleTransform, relativeTo: nil, duration: 5.0)
-            Scale.isEnabled = true
-        default:
-            break
-        }
-    }
-    
-    @objc func handleScaleGesture(_ sender: EntityScaleGestureRecognizer) {
-        print("in scale")
     }
 }
 
