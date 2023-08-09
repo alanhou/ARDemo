73d7db7c956f6f958b0835343d8c4d0576e70b24
alanhou <info@iscbj.com>
2023-08-09 17:22:29 +0800

    代码清单3-7:平移动画

--- ARDemo/ContentView.swift
+++ ARDemo/ContentView.swift
@@ -26,7 +26,7 @@ struct ARViewContainer: UIViewRepresentable {
         config.worldAlignment = .gravity
         arView.session.run(config, options: [])
         arView.session.delegate = arView
-        arView.loadModel()
+        arView.createPlane()
         return arView
     }
     
@@ -34,32 +34,56 @@ struct ARViewContainer: UIViewRepresentable {
     
 }
 
+var cubeEntity: ModelEntity?
+var gestureStartLocation: SIMD3<Float>?
 
 extension ARView: ARSessionDelegate{
-    func loadModel() {
+    func createPlane() {
         let planeAnchor = AnchorEntity(plane: .horizontal)
-//        同步加载
-//        do {
-//            let usdzPath = "toy_drummer"
-//            let modelEntity = try ModelEntity.loadModel(named: usdzPath)
-//            print("加载成功！")
-//            planeAnchor.addChild(modelEntity)
-//        } catch {
-//            print("找不到文件")
-//        }
-//        异步加载
-        let usdzPath = "toy_drummer"
-        var cancellable: AnyCancellable? = nil
-        cancellable = ModelEntity.loadModelAsync(named: usdzPath)
-            .sink(receiveCompletion: { error in
-                print("发生错误：\(error)")
-                cancellable?.cancel()
-            }, receiveValue: { entity in
-                planeAnchor.addChild(entity)
-                cancellable?.cancel()
-            })
-        
-        self.scene.addAnchor(planeAnchor)
+        do {
+            let cubeMesh = MeshResource.generateBox(size: 0.1)
+            var cubeMaterial = SimpleMaterial(color: .white, isMetallic: false)
+            cubeMaterial.color = try .init(texture: .init(.load(named: "Box_Texture")))
+            cubeEntity = ModelEntity(mesh: cubeMesh, materials: [cubeMaterial])
+            cubeEntity?.generateCollisionShapes(recursive: false)
+            cubeEntity?.name = "this is a cube"
+            planeAnchor.addChild(cubeEntity!)
+            self.scene.addAnchor(planeAnchor)
+            self.installGestures(.all, for: cubeEntity!).forEach{
+                $0.addTarget(self, action: #selector(handleModelGesture))
+            }
+        } catch {
+            print("找不到文件")
+        }
+    }
+    
+    @objc func handleModelGesture(_ sender: Any) {
+        switch sender {
+        case let rotation as EntityRotationGestureRecognizer:
+            rotation.isEnabled = false
+            var transform = rotation.entity!.transform
+            transform.rotation = simd_quatf(angle: .pi * 1.5, axis: [0, 1, 0])
+            rotation.entity?.move(to: transform, relativeTo: nil, duration: 5.0)
+            rotation.isEnabled = true
+        case let translation as EntityTranslationGestureRecognizer:
+            translation.isEnabled = false
+            var transform = translation.entity!.transform
+            transform.translation = SIMD3<Float>(x: 0.8, y: 0, z: 0)
+            translation.entity?.move(to: transform, relativeTo: nil, duration: 5.0)
+            translation.isEnabled = true
+        case let Scale as EntityScaleGestureRecognizer:
+            Scale.isEnabled = false
+            var scaleTransform = Scale.entity!.transform
+            scaleTransform.scale = SIMD3<Float>(x: 2, y: 2, z: 2)
+            Scale.entity?.move(to: scaleTransform, relativeTo: nil, duration: 5.0)
+            Scale.isEnabled = true
+        default:
+            break
+        }
+    }
+    
+    @objc func handleScaleGesture(_ sender: EntityScaleGestureRecognizer) {
+        print("in scale")
     }
 }
 
