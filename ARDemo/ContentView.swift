e8d7d9398d746736eafadab0347062e56eb325d8
alanhou <info@iscbj.com>
2023-08-15 17:03:33 +0800

    代码清单6-9:手动控制环境探头

--- ARDemo/ContentView.swift
+++ ARDemo/ContentView.swift
@@ -9,6 +9,15 @@ import SwiftUI
 import RealityKit
 import ARKit
 
+struct ManualProbe {
+    var objectProbeAnchor: AREnvironmentProbeAnchor?
+    var requiresRefresh: Bool = false
+    var lastUpdateTime: TimeInterval = Date().timeIntervalSince1970
+    var dateTime = Date()
+    var sphereEntity: ModelEntity!
+    var isPlanced = false
+}
+
 struct ContentView : View {
     var body: some View {
         ARViewContainer().edgesIgnoringSafeArea(.all)
@@ -22,8 +31,8 @@ struct ARViewContainer: UIViewRepresentable {
         let config = ARWorldTrackingConfiguration()
         config.planeDetection = .horizontal
         arView.automaticallyConfigureSession = false
-        config.environmentTexturing = .automatic
-        config.wantsHDREnvironmentTextures = true
+        config.isLightEstimationEnabled = true
+        config.environmentTexturing = .manual
         arView.session.delegate = arView
         arView.session.run(config, options: [])
         return arView
@@ -33,19 +42,51 @@ struct ARViewContainer: UIViewRepresentable {
     
 }
 
+var manualProbe = ManualProbe()
+
 extension ARView: ARSessionDelegate{
     public func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
-        guard let anchor = anchors.first as? ARPlaneAnchor else {return}
+        guard let anchor = anchors.first as? ARPlaneAnchor, !manualProbe.isPlanced else {return}
         let planeAnchor = AnchorEntity(anchor: anchor)
         let sphereRadius: Float = 0.1
         let sphere: MeshResource = .generateSphere(radius: sphereRadius)
         let sphereMaterial = SimpleMaterial(color: .blue, isMetallic: true)
-        let sphereEntity = ModelEntity(mesh: sphere, materials: [sphereMaterial])
-        sphereEntity.transform.translation = [0, planeAnchor.transform.translation.y+0.05, 0]
-        planeAnchor.addChild(sphereEntity)
+        manualProbe.sphereEntity = ModelEntity(mesh: sphere, materials: [sphereMaterial])
+        manualProbe.sphereEntity.transform.translation = [0, planeAnchor.transform.translation.y+0.05, 0]
+        manualProbe.requiresRefresh = true
+        updateProbe()
+        planeAnchor.addChild(manualProbe.sphereEntity)
         self.scene.addAnchor(planeAnchor)
-        self.session.delegate = nil
         self.session.run(ARWorldTrackingConfiguration())
+    }
+    
+    public func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        if manualProbe.requiresRefresh && (manualProbe.dateTime.timeIntervalSince1970 - manualProbe.lastUpdateTime > 1) {
+            manualProbe.lastUpdateTime = manualProbe.dateTime.timeIntervalSince1970
+            updateProbe()
+        }
+    }
+    
+    func updateProbe() {
+        if let probeAnchor = manualProbe.objectProbeAnchor {
+            self.session.remove(anchor: probeAnchor)
+            manualProbe.objectProbeAnchor = nil
+        }
+        var extent = (manualProbe.sphereEntity.model?.mesh.bounds.extents)! * manualProbe.sphereEntity.transform.scale
+        extent.x *= 3
+        extent.z *= 3
+        let verticalOffset = SIMD3(0, extent.y, 0)
+        var probeTransform = manualProbe.sphereEntity.transform
+        probeTransform.translation += verticalOffset
+        let position = simd_float4x4(
+            SIMD4(1, 0, 0, 0),
+            SIMD4(0, 1, 0, 0),
+            SIMD4(0, 0, 1, 0),
+            SIMD4(manualProbe.sphereEntity.transform.translation, 1)
+        )
+        extent.y *= 2
+        manualProbe.objectProbeAnchor = AREnvironmentProbeAnchor(name: "objectProbe", transform: position, extent: extent)
+        self.session.add(anchor: manualProbe.objectProbeAnchor!)
     }
 }
 
