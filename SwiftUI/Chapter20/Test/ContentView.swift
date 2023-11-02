d6c5e548b76d01cd45583e70a2377a31fdb5cee7
alanhou <info@iscbj.com>
2023-11-02 21:02:13 +0800

    Listing 20-1: Testing String Catalogs

---
+++ SwiftUI/Chapter20/Test/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Alan on 2023/11/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello World!")
+                .padding()
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
