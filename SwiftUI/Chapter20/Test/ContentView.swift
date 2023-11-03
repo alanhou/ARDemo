6a7d2d66981ba1173b8b6b2ec84b6982f626e1e6
alanhou <info@iscbj.com>
2023-11-03 10:02:21 +0800

    示例20-3：添加供翻译查看的注释

--- SwiftUI/Chapter20/Test/ContentView.swift
+++ SwiftUI/Chapter20/Test/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Hello World!")
+            Text("Hello World!", comment: "This is a welcome message")
                 .padding()
             Spacer()
         }
