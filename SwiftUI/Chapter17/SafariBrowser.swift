9877d3fb8975bc7543c3ea274d6d9bd18d77beee
alanhou <info@iscbj.com>
2023-11-08 11:06:27 +0800

    示例17-6：配置视图控制器

--- SwiftUI/Chapter17/SafariBrowser.swift
+++ SwiftUI/Chapter17/SafariBrowser.swift
@@ -13,6 +13,9 @@ struct SafariBrowser: UIViewControllerRepresentable {
     
     func makeUIViewController(context: Context) -> SFSafariViewController {
         let safari = SFSafariViewController(url: searchURL)
+        safari.dismissButtonStyle = .close
+        safari.preferredBarTintColor = UIColor(red: 81/255, green: 91/255, blue: 119/255, alpha: 1.0)
+        safari.preferredControlTintColor = UIColor.white
         return safari
     }
     func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {
