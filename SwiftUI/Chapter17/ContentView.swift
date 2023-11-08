673afc554e1033adb383bc2c8e21f9e1712e6c53
alanhou <info@iscbj.com>
2023-11-08 11:48:32 +0800

    示例17-9：通过Safari视图控制器代理禁用按钮

--- SwiftUI/Chapter17/ContentView.swift
+++ SwiftUI/Chapter17/ContentView.swift
@@ -10,16 +10,18 @@ import SwiftUI
 struct ContentView: View {
     @State private var searchURL: URL = URL(string: "https://www.formasterminds.com")!
     @State private var openSheet: Bool = false
+    @State private var disableButton: Bool = false
     
     var body: some View {
         VStack {
             Button("Open Browser") {
                 openSheet = true
             }.buttonStyle(.borderedProminent)
+                .disabled(disableButton)
             Spacer()
         }.padding()
             .sheet(isPresented: $openSheet) {
-                SafariBrowser(searchURL: $searchURL)
+                SafariBrowser(disable: $disableButton, searchURL: $searchURL)
             }
     }
 }
