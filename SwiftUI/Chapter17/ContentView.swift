d58571c32ede4fd656e2112ce0f99b688b1a4906
alanhou <info@iscbj.com>
2023-11-08 17:11:23 +0800

    示例17-11：显示WebKit视图

--- SwiftUI/Chapter17/ContentView.swift
+++ SwiftUI/Chapter17/ContentView.swift
@@ -8,21 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var searchURL: URL = URL(string: "https://www.formasterminds.com")!
-    @State private var openSheet: Bool = false
-    @State private var disableButton: Bool = false
-    
     var body: some View {
-        VStack {
-            Button("Open Browser") {
-                openSheet = true
-            }.buttonStyle(.borderedProminent)
-                .disabled(disableButton)
-            Spacer()
-        }.padding()
-            .sheet(isPresented: $openSheet) {
-                SafariBrowser(disable: $disableButton, searchURL: $searchURL)
-            }
+        WebView(searchURL: URL(string: "https://www.google.com")!)
     }
 }
 
