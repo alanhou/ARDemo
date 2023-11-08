2038b8995a550dfb10a7ad08ca8afa8fc5430156
alanhou <info@iscbj.com>
2023-11-08 11:44:02 +0800

    示例17-8：为Safari视图控制器添加代理

--- SwiftUI/Chapter17/SafariBrowser.swift
+++ SwiftUI/Chapter17/SafariBrowser.swift
@@ -9,14 +9,30 @@ import SwiftUI
 import SafariServices
 
 struct SafariBrowser: UIViewControllerRepresentable {
+    @Binding var disable: Bool
     @Binding var searchURL: URL
     
     func makeUIViewController(context: Context) -> SFSafariViewController {
         let config = SFSafariViewController.Configuration()
         config.barCollapsingEnabled = false
         let safari = SFSafariViewController(url: searchURL, configuration: config)
+        safari.delegate = context.coordinator
         return safari
     }
     func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {
+    }
+    func makeCoordinator() -> SafariCoordinator {
+        SafariCoordinator(disableCoordinator: $disable)
+    }
+}
+
+class SafariCoordinator: NSObject, SFSafariViewControllerDelegate {
+    @Binding var disableCoordinator: Bool
+    
+    init(disableCoordinator: Binding<Bool>) {
+        self._disableCoordinator = disableCoordinator
+    }
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        disableCoordinator = true
     }
 }
