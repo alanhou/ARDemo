9e6ba308ea2e15a6fc9f4af2c53959310a4fc008
alanhou <info@iscbj.com>
2023-11-09 21:16:55 +0800

    示例17-17：显示文档内容

--- SwiftUI/Chapter17/ContentView.swift
+++ SwiftUI/Chapter17/ContentView.swift
@@ -8,51 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var webView: WebView!
-    @State private var inputURL: String = ""
-    @State private var backDisabled: Bool = true
-    @State private var forwardDisabled: Bool = true
+    @Environment(ApplicationData.self) private var appData
     
     var body: some View {
         VStack {
-            HStack {
-                TextField("Insert URL", text: $inputURL)
-                Button("Load") {
-                    let text = inputURL.trimmingCharacters(in: .whitespaces)
-                    if !text.isEmpty {
-                        webView.loadWeb(web: text)
-                    }
+            Button("Load Web") {
+                Task(priority: .high) {
+                    await appData.loadWeb()
                 }
-            }.padding(5)
+            }.disabled(appData.buttonDisabled)
             
-            HStack {
-                Button(action: {
-                    webView.goBack()
-                }, label: {
-                    Image(systemName: "arrow.left.circle")
-                        .font(.title)
-                }).disabled(backDisabled)
-                Button(action: {
-                    webView.goForward()
-                }, label: {
-                    Image(systemName: "arrow.right.circle")
-                        .font(.title)
-                }).disabled(forwardDisabled)
-                Spacer()
-                Button(action: {
-                    webView.refresh()
-                }, label: {
-                    Image(systemName: "arrow.clockwise.circle")
-                        .font(.title)
-                })
-            }.padding(5)
-            webView
-        }.onAppear {
-            webView = WebView(inputURL: $inputURL, backDisabled: $backDisabled, forwardDisabled: $forwardDisabled)
-        }
+            Text("Total Characters: \(appData.webContent.count)")
+                .padding()
+            Spacer()
+        }.padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView().environment(ApplicationData())
 }
