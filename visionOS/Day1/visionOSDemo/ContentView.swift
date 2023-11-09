66df21a61f09f9005277e028da54721c0daddafb
alanhou <info@iscbj.com>
2023-11-09 12:53:48 +0800

    Day 1：环境安装和编写第一个程序

---
+++ visionOS/Day1/visionOSDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  visionOSDemo
+//
+//  Created by Alan on 2023/11/9.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, visionOS!")
+        }
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
