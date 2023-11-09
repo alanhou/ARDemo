593d9323909f7d18240e4bf972c9f45091c8e5eb
alanhou <info@iscbj.com>
2023-11-09 21:04:26 +0800

    示例17-16：加载远程文档

---
+++ SwiftUI/Chapter17/ApplicationData.swift
@@ -0,0 +1,40 @@
+//
+//  ApplicationData.swift
+//  Test
+//
+//  Created by Alan on 2023/11/9.
+//
+
+import SwiftUI
+import Observation
+
+@Observable class ApplicationData {
+    var webContent: String = ""
+    var buttonDisabled: Bool = false
+    
+    func loadWeb() async {
+        buttonDisabled = true
+        
+        let session = URLSession.shared
+        let webURL = URL(string: "https://www.yahoo.com")
+        do {
+            let (data, response) = try await session.data(from: webURL!)
+            if let resp = response as? HTTPURLResponse {
+                let status = resp.statusCode
+                if status == 200 {
+                    if let content = String(data: data, encoding: String.Encoding.ascii) {
+                        await MainActor.run {
+                            webContent = content
+                            buttonDisabled = false
+                        }
+                        print(content)
+                    }
+                } else {
+                    print("Error: \(status)")
+                }
+            }
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+}
