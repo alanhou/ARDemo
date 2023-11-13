769eba47594a357684fc7fe32c2fa2d2513d4455
alanhou <info@iscbj.com>
2023-11-13 16:56:26 +0800

    示例9-18：异步加载图片

--- SwiftUI/Chapter09/ContentView.swift
+++ SwiftUI/Chapter09/ContentView.swift
@@ -8,35 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    let website = URL(string: "http://alanhou.org/homepage/wp-content/uploads/2019/03/201903251411121.jpg")
     var body: some View {
         VStack {
-          Text("Hello World!")
-                .padding()
-        }.onAppear {
-            Task(priority: .background) {
-                await withTaskGroup(of: String.self) { group in
-                    group.addTask(priority: .background) {
-                        let imageName = await self.loadImage(name: "image1")
-                        return imageName
-                    }
-                    group.addTask(priority: .background) {
-                        let imageName = await self.loadImage(name: "image2")
-                        return imageName
-                    }
-                    group.addTask(priority: .background) {
-                        let imageName = await self.loadImage(name: "image3")
-                        return imageName
-                    }
-                    for await result in group {
-                        print(result)
-                    }
-                }
-            }
-        }
-    }
-    func loadImage(name: String) async -> String {
-        try? await Task.sleep(nanoseconds: 3 * 1000000000)
-        return "Name: \(name)"
+            AsyncImage(url: website)
+        }.padding()
     }
 }
 
