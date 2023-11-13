d77e5b8a4d95ae7f6bca3ed3a4d4070d41ce939a
alanhou <info@iscbj.com>
2023-11-13 13:20:17 +0800

    示例9-16：定义一个异步序列

--- SwiftUI/Chapter09/ContentView.swift
+++ SwiftUI/Chapter09/ContentView.swift
@@ -7,25 +7,45 @@
 
 import SwiftUI
 
+struct ImageIterator: AsyncIteratorProtocol {
+    let imageList: [String]
+    var current = 0
+    
+    mutating func next() async -> String? {
+        guard current < imageList.count else {
+            return nil
+        }
+        try? await Task.sleep(nanoseconds: 3 * 1000000000)
+        let image = imageList[current]
+        current += 1
+        return image
+    }
+}
+
+struct ImageLoader: AsyncSequence {
+    typealias Element = String
+    let imageList: [String]
+    
+    func makeAsyncIterator() -> ImageIterator {
+        return AsyncIterator(imageList: imageList)
+    }
+}
+
 struct ContentView: View {
-    @State private var myText: String = "Hello, world!"
+    let list = ["image1", "image2", "image3"]
     
     var body: some View {
         VStack {
-            Text(myText)
+          Text("Hello World!")
                 .padding()
         }.onAppear {
             Task(priority: .background) {
-                await loadImage(name: "image1")
+                let loader = ImageLoader(imageList: list)
+                for await image in loader {
+                    print(image)
+                }
             }
         }
-    }
-    func loadImage(name: String) async {
-        let result: String = await MainActor.run {
-            myText = name
-            return "Name: \(name)"
-        }
-        print(result)
     }
 }
 
