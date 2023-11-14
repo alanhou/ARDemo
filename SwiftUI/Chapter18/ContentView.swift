16afd3df749e07438b8fe37a56a8ead584cab674
alanhou <info@iscbj.com>
2023-11-14 20:13:55 +0800

    示例18-1：创建一个图片选择器

---
+++ SwiftUI/Chapter18/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Alan on 2023/11/2.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct ContentView: View {
+    @State private var selected: PhotosPickerItem?
+    @State private var picture: UIImage?
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Image(uiImage: picture ?? UIImage(named: "nopicture")!)
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+                PhotosPicker(selection: $selected, matching: .images, photoLibrary: .shared()) {
+                    Text("Select a photo")
+                        .padding()
+                        .buttonStyle(.borderedProminent)
+                }
+                .onChange(of: selected, initial: false) { old, item in
+                    Task(priority: .background) {
+                        if let data = try? await item?.loadTransferable(type: Data.self) {
+                            picture = UIImage(data: data)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
