b0d5a9efd484fd4c2eee51cebfdd2d6e0b3eb787
alanhou <info@iscbj.com>
2023-11-22 09:25:40 +0800

    示例18-8：在保存图片时显示警告视图

--- SwiftUI/Chapter18/ContentView.swift
+++ SwiftUI/Chapter18/ContentView.swift
@@ -11,17 +11,30 @@ import PhotosUI
 struct ContentView: View {
     @State private var path = NavigationPath()
     @State private var picture: UIImage?
-    
+    @State private var showAlert: Bool = false
     
     var body: some View {
         NavigationStack(path: $path) {
             VStack {
                 HStack {
+                    Button("Share Picture") {
+                        showAlert = true
+                    }.disabled(picture == nil ? true : false)
                     Spacer()
                     NavigationLink("Get Picture", value: "Open Picker")
                 }.navigationDestination(for: String.self, destination: { _ in
                     ImagePicker(path: $path, picture: $picture)
                 })
+                .alert("Save Picture", isPresented: $showAlert, actions: {
+                    Button("Cancel", role: .cancel, action: {
+                        showAlert = false
+                    })
+                    Button("YES", role: .none, action: {
+                        if let picture {
+                            UIImageWriteToSavedPhotosAlbum(picture, nil, nil, nil)
+                        }
+                    })
+                }, message: { Text("Do you want to store the picture in the Photo Library?") })
                 Image(uiImage: picture ?? UIImage(named: "nopicture")!)
                     .resizable()
                     .scaledToFit()
