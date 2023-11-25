cd3d716ccfc4c2ae87799778cdbf220128746499
alanhou <info@iscbj.com>
2023-11-25 12:28:11 +0800

    示例18-9：对其它应用分享图像

--- SwiftUI/Chapter18/ContentView.swift
+++ SwiftUI/Chapter18/ContentView.swift
@@ -11,33 +11,23 @@ import PhotosUI
 struct ContentView: View {
     @State private var path = NavigationPath()
     @State private var picture: UIImage?
-    @State private var showAlert: Bool = false
     
     var body: some View {
         NavigationStack(path: $path) {
             VStack {
                 HStack {
-                    Button("Share Picture") {
-                        showAlert = true
-                    }.disabled(picture == nil ? true : false)
+                    if let picture = picture {
+                        let photo = Image(uiImage: picture)
+                        ShareLink("Share Picture", item: photo, preview: SharePreview("Photo", image: photo))
+                    }
                     Spacer()
                     NavigationLink("Get Picture", value: "Open Picker")
                 }.navigationDestination(for: String.self, destination: { _ in
                     ImagePicker(path: $path, picture: $picture)
                 })
-                .alert("Save Picture", isPresented: $showAlert, actions: {
-                    Button("Cancel", role: .cancel, action: {
-                        showAlert = false
-                    })
-                    Button("YES", role: .none, action: {
-                        if let picture {
-                            UIImageWriteToSavedPhotosAlbum(picture, nil, nil, nil)
-                        }
-                    })
-                }, message: { Text("Do you want to store the picture in the Photo Library?") })
                 Image(uiImage: picture ?? UIImage(named: "nopicture")!)
                     .resizable()
-                    .scaledToFit()
+                    .scaledToFill()
                     .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
                     .clipped()
                 Spacer()
