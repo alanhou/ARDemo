180d77c14308533b7bc5b9d249c3183f96d91dff
alanhou <info@iscbj.com>
2023-11-27 14:07:55 +0800

    示例18-17：显示图像

--- SwiftUI/Chapter18/ContentView.swift
+++ SwiftUI/Chapter18/ContentView.swift
@@ -9,33 +9,30 @@ import SwiftUI
 import PhotosUI
 
 struct ContentView: View {
-    @State private var path = NavigationPath()
-    @State private var picture: UIImage?
+    @Environment(ApplicatonData.self) private var appData
     
     var body: some View {
-        NavigationStack(path: $path) {
+        NavigationStack(path: Bindable(appData).path) {
             VStack {
                 HStack {
-                    if let picture = picture {
-                        let photo = Image(uiImage: picture)
-                        ShareLink("Share Picture", item: photo, preview: SharePreview("Photo", image: photo))
-                    }
                     Spacer()
-                    NavigationLink("Get Picture", value: "Open Picker")
+                    NavigationLink("Take Picture", value: "Open Camera")
                 }.navigationDestination(for: String.self, destination: { _ in
-                    ImagePicker(path: $path, picture: $picture)
+                    CustomCameraView()
                 })
-                Image(uiImage: picture ?? UIImage(named: "nopicture")!)
+                Image(uiImage: appData.picture ?? UIImage(named: "nopicture")!)
                     .resizable()
                     .scaledToFill()
                     .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
                     .clipped()
                 Spacer()
             }.padding()
+                .navigationBarHidden(true)
         }.statusBarHidden()
     }
 }
 
 #Preview {
     ContentView()
+        .environment(ApplicatonData())
 }
