76a9311cdff94701e57276fc9300d8e0f47f76ab
alanhou <info@iscbj.com>
2023-11-27 16:49:46 +0800

    Day 9 打造“任意门”

---
+++ visionOS/Day9/visionOSDemo/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  visionOSDemo
+//
+//  Created by Alan on 2023/11/21.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView: View {
+    var body: some View {
+        RealityView { content in
+            let world = makeWorld()
+            let portal = makePortal(world: world)
+            content.add(world)
+            content.add(portal)
+        }
+    }
+    
+    func makeWorld() -> Entity {
+        let world = Entity()
+        world.components[WorldComponent.self] = .init()
+        
+        let environment = try! EnvironmentResource.load(named: "Sunlight")
+        world.components[ImageBasedLightComponent.self] = .init(source: .single(environment), intensityExponent: 12)
+        world.components[ImageBasedLightReceiverComponent.self] = .init(imageBasedLight: world)
+        
+        let earth = try! ModelEntity.load(named: "Earth")
+        earth.position = SIMD3<Float>(x: -0.1, y: 0, z: -0.4)
+        world.addChild(earth)
+        
+        return world
+    }
+    
+    func makePortal(world: Entity) -> Entity {
+        let portal = Entity()
+        
+        portal.components[ModelComponent.self] = .init(mesh: .generatePlane(width: 0.5, height: 0.5, cornerRadius: 0.5), materials: [PortalMaterial()])
+        portal.components[PortalComponent.self] = .init(target: world)
+        
+        return portal
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
