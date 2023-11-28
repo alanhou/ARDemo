e7d4ed5c08564039b6ebb960ddc34e7e34b44fc5
alanhou <info@iscbj.com>
2023-11-28 19:51:40 +0800

    示例18-22：在视频上展示视图

--- SwiftUI/Chapter18/ContentView.swift
+++ SwiftUI/Chapter18/ContentView.swift
@@ -13,10 +13,19 @@ struct ContentView: View {
     
     var body: some View {
         if appData.player != nil {
-            VideoPlayer(player: appData.player)
-                .onAppear {
-                    appData.player.play()
+            VideoPlayer(player: appData.player, videoOverlay: {
+                VStack {
+                    Text("Title: Trees at the park")
+                        .font(.title)
+                        .padding([.top, .bottom], 8)
+                        .padding([.leading, .trailing], 16)
+                        .foregroundColor(.black)
+                        .background(.ultraThinMaterial)
+                        .cornerRadius(10)
+                        .padding(.top, 8)
+                    Spacer()
                 }
+            })
                 .ignoresSafeArea()
         } else {
             Text("Video not available")
