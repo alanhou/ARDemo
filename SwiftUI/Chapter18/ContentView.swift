88efbf05394b285655e14dd83215cd6f1699ec2c
alanhou <info@iscbj.com>
2023-11-28 19:33:26 +0800

    示例18-20：播放视频

--- SwiftUI/Chapter18/ContentView.swift
+++ SwiftUI/Chapter18/ContentView.swift
@@ -6,29 +6,18 @@
 //
 
 import SwiftUI
-import PhotosUI
+import AVKit
 
 struct ContentView: View {
     @Environment(ApplicationData.self) private var appData
     
     var body: some View {
-        NavigationStack(path: Bindable(appData).path) {
-            VStack {
-                HStack {
-                    Spacer()
-                    NavigationLink("Take Picture", value: "Open Camera")
-                }.navigationDestination(for: String.self, destination: { _ in
-                    CustomCameraView()
-                })
-                Image(uiImage: appData.picture ?? UIImage(named: "nopicture")!)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                    .clipped()
-                Spacer()
-            }.padding()
-                .navigationBarHidden(true)
-        }.statusBarHidden()
+        if appData.player != nil {
+            VideoPlayer(player: appData.player)
+                .ignoresSafeArea()
+        } else {
+            Text("Video not available")
+        }
     }
 }
 
