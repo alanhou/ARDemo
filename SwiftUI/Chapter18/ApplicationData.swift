f36dd92fdaa9bab7d164dca4b5293635f5e858fc
alanhou <info@iscbj.com>
2023-12-04 11:42:51 +0800

    示例18-24：构建自定义视频播放器

--- SwiftUI/Chapter18/ApplicationData.swift
+++ SwiftUI/Chapter18/ApplicationData.swift
@@ -7,15 +7,39 @@
 
 import SwiftUI
 import Observation
-import AVKit
+import AVFoundation
+
+class ViewData: NSObject {
+    var playerItem: AVPlayerItem?
+    var player: AVPlayer?
+    var playerLayer: AVPlayerLayer?
+    var playerObservation: NSKeyValueObservation?
+    
+    func setObserver() {
+        playerObservation = playerItem?.observe(\.status, options: .new, changeHandler: { item, value in
+            if item.status == .readyToPlay {
+                self.player?.play()
+            }
+        })
+    }
+}
 
 @Observable class ApplicationData {
-    var player: AVPlayer!
+    @ObservationIgnored var customVideoView: PlayerView!
+    @ObservationIgnored var viewData: ViewData
     
     init() {
+        customVideoView = PlayerView()
+        viewData = ViewData()
+        
         let bundle = Bundle.main
-        if let videoURL = bundle.url(forResource: "videotrees", withExtension: "mp4") {
-            player = AVPlayer(url: videoURL)
-        }
+        let videoURL = bundle.url(forResource: "videotrees", withExtension: "mp4")
+        let asset = AVURLAsset(url: videoURL!)
+        viewData.playerItem = AVPlayerItem(asset: asset)
+        viewData.player = AVPlayer(playerItem: viewData.playerItem)
+        
+        viewData.playerLayer = customVideoView.view.layer as? AVPlayerLayer
+        viewData.playerLayer?.player = viewData.player
+        viewData.setObserver()
     }
 }
