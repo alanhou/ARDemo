fd189e17d8a1258a07046c9bb3c3daf2ecdc97e3
alanhou <info@iscbj.com>
2023-12-04 12:58:22 +0800

    示例18-25：显示视频

--- SwiftUI/Chapter18/ContentView.swift
+++ SwiftUI/Chapter18/ContentView.swift
@@ -12,24 +12,8 @@ struct ContentView: View {
     @Environment(ApplicationData.self) private var appData
     
     var body: some View {
-        if appData.player != nil {
-            VideoPlayer(player: appData.player, videoOverlay: {
-                VStack {
-                    Text("Title: Trees at the park")
-                        .font(.title)
-                        .padding([.top, .bottom], 8)
-                        .padding([.leading, .trailing], 16)
-                        .foregroundColor(.black)
-                        .background(.ultraThinMaterial)
-                        .cornerRadius(10)
-                        .padding(.top, 8)
-                    Spacer()
-                }
-            })
-                .ignoresSafeArea()
-        } else {
-            Text("Video not available")
-        }
+        appData.customVideoView
+            .ignoresSafeArea()
     }
 }
 
