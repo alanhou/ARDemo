170adab07fefce152212a97ad456a9ba2cdec75e
alanhou <info@iscbj.com>
2023-12-05 20:23:01 +0800

    示例12-3：监测长按手势

--- SwiftUI/Chapter12/ContentView.swift
+++ SwiftUI/Chapter12/ContentView.swift
@@ -10,16 +10,21 @@ import AVKit
 
 struct ContentView: View {
     @State private var expand: Bool = false
+    @State private var pressing: Bool = false
     
     var body: some View {
         Image(.spot1)
             .resizable()
             .scaledToFit()
             .frame(width: 160, height: 200)
-            .onTapGesture { location in
+            .opacity(pressing ? 0 : 1)
+            .onLongPressGesture(minimumDuration: 1, maximumDistance: 10,perform: {
                 expand = true
-                print("Location: \(location)")
-            }
+            }, onPressingChanged: { value in
+                withAnimation(.easeInOut(duration: 1.5)) {
+                    pressing = value
+                }
+            })
             .sheet(isPresented: $expand) {
                 ShowImage()
             }
