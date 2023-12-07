4529561bb56d3fad878f564694f2c0ec08cfd1a5
alanhou <info@iscbj.com>
2023-12-07 10:48:50 +0800

    示例12-5：定义内容区

--- SwiftUI/Chapter12/ContentView.swift
+++ SwiftUI/Chapter12/ContentView.swift
@@ -8,24 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var expand: Bool = false
-    @State private var allowExpansion: Bool = false
+    @State private var selected: Bool = false
     
     var body: some View {
-        VStack(spacing: 20) {
-            Image(.spot1)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 160, height: 200)
-                .onTapGesture {
-                    expand = true
+        VStack {
+            HStack(alignment: .top) {
+                Image(.spot1)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80, height: 100)
+                    .border(selected ? Color.yellow : Color.clear, width: 5)
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("Balmy Beach").bold()
+                    Text("Toronto")
+                    Text("2020").font(.caption)
+                    Spacer()
                 }
-                .allowsHitTesting(allowExpansion)
-                .sheet(isPresented: $expand) {
-                    ShowImage()
-            }
-            Toggle("", isOn: $allowExpansion)
-                .labelsHidden()
+                Spacer()
+            }.frame(height: 100)
+                .padding(5)
+                .border(.gray, width: 1)
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    selected.toggle()
+                }
+            Spacer()
         }
     }
 }
