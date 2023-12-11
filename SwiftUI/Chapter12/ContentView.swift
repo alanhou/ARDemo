55e94ebab2a17b940b49d17511526557fcc73dec
alanhou <info@iscbj.com>
2023-12-11 14:17:32 +0800

    示例12-6：定义一个TapGesture识别器

--- SwiftUI/Chapter12/ContentView.swift
+++ SwiftUI/Chapter12/ContentView.swift
@@ -8,32 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selected: Bool = false
+    @State private var expand: Bool = false
     
     var body: some View {
-        VStack {
-            HStack(alignment: .top) {
-                Image(.spot1)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 80, height: 100)
-                    .border(selected ? Color.yellow : Color.clear, width: 5)
-                VStack(alignment: .leading, spacing: 2) {
-                    Text("Balmy Beach").bold()
-                    Text("Toronto")
-                    Text("2020").font(.caption)
-                    Spacer()
-                }
-                Spacer()
-            }.frame(height: 100)
-                .padding(5)
-                .border(.gray, width: 1)
-                .contentShape(Rectangle())
-                .onTapGesture {
-                    selected.toggle()
-                }
-            Spacer()
-        }
+        Image(.spot1)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 160, height: 200)
+            .gesture(
+                TapGesture(count: 1)
+                    .onEnded {
+                        expand = true
+                    }
+            )
+            .sheet(isPresented: $expand) {
+                ShowImage()
+            }
     }
 }
 
