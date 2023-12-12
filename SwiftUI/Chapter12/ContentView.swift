4328d9c91659f7a3834f186f78dcb8052169c412
alanhou <info@iscbj.com>
2023-12-12 15:22:30 +0800

    示例12-7：定义LongPressGesture识别器

--- SwiftUI/Chapter12/ContentView.swift
+++ SwiftUI/Chapter12/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @GestureState private var pressing: Bool = false
     @State private var expand: Bool = false
     
     var body: some View {
@@ -15,9 +16,14 @@ struct ContentView: View {
             .resizable()
             .scaledToFit()
             .frame(width: 160, height: 200)
+            .opacity(pressing ? 0 : 1)
             .gesture(
-                TapGesture(count: 1)
-                    .onEnded {
+                LongPressGesture(minimumDuration: 1)
+                    .updating($pressing) { value, state, transaction in
+                        state = value
+                        transaction.animation = Animation.easeInOut(duration: 1.5)
+                    }
+                    .onEnded { value in
                         expand = true
                     }
             )
