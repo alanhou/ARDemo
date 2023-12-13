07e599ac88eb1345c5537a5b216e2a662041efe5
alanhou <info@iscbj.com>
2023-12-13 20:02:43 +0800

    示例12-12：允许用户拖拽图片

--- SwiftUI/Chapter12/ContentView.swift
+++ SwiftUI/Chapter12/ContentView.swift
@@ -8,23 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @GestureState private var rotationAngle: Angle = Angle.zero
-    @State private var rotation: Angle = Angle.zero
-    
     var body: some View {
-        Image(.spot1)
-            .resizable()
-            .scaledToFit()
-            .frame(width: 160, height: 200)
-            .rotationEffect(rotation + rotationAngle)
-            .gesture(RotationGesture()
-                .updating($rotationAngle) { value, state, transaction in
-                    state = value
-                }
-                .onEnded { value in
-                    rotation = rotation + value
-                }
-            )
+        VStack {
+            Image(.husky)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 400)
+                .draggable(Image(.husky))
+            Spacer()
+        }
     }
 }
 
