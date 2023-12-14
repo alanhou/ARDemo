4e3cfacd5336c19809cfffb0d3c74ce5fac0f19e
alanhou <info@iscbj.com>
2023-12-14 13:06:46 +0800

    示例12-15：向用户提供反馈

--- SwiftUI/Chapter12/ContentView.swift
+++ SwiftUI/Chapter12/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var picture: Image = Image(.nopicture)
+    @State private var didEnter: Bool = false
     var body: some View {
         VStack {
             picture
@@ -16,12 +17,15 @@ struct ContentView: View {
                 .scaledToFit()
                 .frame(minWidth: 0, maxWidth: .infinity)
                 .frame(height: 400)
+                .overlay(didEnter ? Color.green.opacity(0.2) : Color.clear)
                 .dropDestination(for: Image.self, action: { elements, location in
                     if let image = elements.first {
                         picture = image
                         return true
                     }
                     return false
+                }, isTargeted: { value in
+                    didEnter = value
                 })
             Spacer()
         }
