8cd88c24dd1fad5e295b2f17b90e7bb0eba8b7e9
alanhou <info@iscbj.com>
2023-12-27 09:10:25 +0800

    示例6-19：为按钮添加自定义样式

--- SwiftUI/Chapter06/ContentView.swift
+++ SwiftUI/Chapter06/ContentView.swift
@@ -7,21 +7,26 @@
 
 import SwiftUI
 
+struct MyStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        let pressed = configuration.isPressed
+        return configuration.label
+            .padding()
+            .border(Color.green, width: 5)
+            .scaleEffect(pressed ? 1.2 : 1.0)
+    }
+}
+
 struct ContentView: View {
+    @State private var color = Color.gray
+    
     var body: some View {
         VStack(spacing: 10) {
-            Button(action: {
-                print("Send information")
-            }, label: {
-                HStack {
-                    Image(systemName: "mail")
-                        .imageScale(.large)
-                    Text("Send")
-                }
-            })
-            .buttonStyle(.borderedProminent)
-            .font(.largeTitle)
-            .controlSize(.large)
+            Text("Default title")
+                .padding().foregroundColor(color)
+            Button("Change Color") {
+                color = Color.green
+            }.buttonStyle(MyStyle())
             Spacer()
         }.padding()
     }
