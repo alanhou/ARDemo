8dfaf22615176b6867fed4021e995d3ca0be360f
alanhou <info@iscbj.com>
2024-01-16 17:31:57 +0800

    示例6-41：创建一个步进器

--- SwiftUI/Chapter06/ContentView.swift
+++ SwiftUI/Chapter06/ContentView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentValue: Float = 5
+    @State private var currentValue: Float = 0
     
     var body: some View {
         VStack {
-            ProgressView()
-                .progressViewStyle(.circular)
+            Text("Current Value: \(currentValue.formatted(.number.precision(.fractionLength(0))))")
+            Stepper("Counter", value: $currentValue, in: 0...100)
             Spacer()
         }.padding()
     }
