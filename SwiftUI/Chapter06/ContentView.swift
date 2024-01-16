59cb0fcb5561f765e79746a5aad8ebda9ce1f4d9
alanhou <info@iscbj.com>
2024-01-16 16:27:57 +0800

    示例6-38：显示进度

--- SwiftUI/Chapter06/ContentView.swift
+++ SwiftUI/Chapter06/ContentView.swift
@@ -9,14 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var currentValue: Float = 5
-    @State private var textAcitve: Bool = false
     
     var body: some View {
         VStack {
-            Text("Current Value: \(currentValue.formatted(.number.precision(.fractionLength(0))))")
-                .padding()
-                .background(textAcitve ? Color.yellow : Color.clear)
-            Slider(value: $currentValue, in: 0...10, step: 1.0, onEditingChanged: { self.textAcitve = $0 })
+            ProgressView(value: currentValue, total: 10)
             Spacer()
         }.padding()
     }
