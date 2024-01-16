da0ae5e64e16829b95a462359afa7b3bdb70443b
alanhou <info@iscbj.com>
2024-01-16 18:35:19 +0800

    示例6-45：在可观测对象中存储数据

---
+++ SwiftUI/Chapter06/ApplicationData.swift
@@ -0,0 +1,14 @@
+//
+//  ApplicationData.swift
+//  Test
+//
+//  Created by Alan on 2024/1/16.
+//
+
+import SwiftUI
+import Observation
+
+@Observable class ApplicationData {
+    var title: String = "Default Title"
+    var titleInput: String = ""
+}
