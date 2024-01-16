bce0bcc8afbac0cc8f607ab0ff496659d9c3441a
alanhou <info@iscbj.com>
2024-01-16 16:07:56 +0800

    示例6-37：响应滑块状态

--- SwiftUI/Chapter06/ContentView.swift
+++ SwiftUI/Chapter06/ContentView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var currentValue: Float = 5
+    @State private var textAcitve: Bool = false
     
     var body: some View {
         VStack {
             Text("Current Value: \(currentValue.formatted(.number.precision(.fractionLength(0))))")
-            Slider(value: $currentValue, in: 0...10, step: 1.0)
+                .padding()
+                .background(textAcitve ? Color.yellow : Color.clear)
+            Slider(value: $currentValue, in: 0...10, step: 1.0, onEditingChanged: { self.textAcitve = $0 })
             Spacer()
         }.padding()
     }
