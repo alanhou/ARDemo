2f4e0d0726587fe2964d6250a94b391876c18595
alanhou <info@iscbj.com>
2024-01-17 14:59:41 +0800

    示例6-55：通过Bindable结构体创建一个可绑定属性

--- SwiftUI/Chapter06/ContentView.swift
+++ SwiftUI/Chapter06/ContentView.swift
@@ -12,18 +12,10 @@ struct ContentView: View {
     @Environment(ApplicationData.self) private var appData
     
     var body: some View {
-        MyInputView(appData: appData)
-    }
-}
-
-struct MyInputView: View {
-    @Bindable var appData: ApplicationData
-    
-    var body: some View {
         VStack(spacing: 8) {
             Text(appData.title)
                 .padding(10)
-            TextField("Insert Title", text: $appData.titleInput)
+            TextField("Insert Title", text: Bindable(appData).titleInput)
                 .textFieldStyle(.roundedBorder)
             Button(action: {
                 appData.title = appData.titleInput
