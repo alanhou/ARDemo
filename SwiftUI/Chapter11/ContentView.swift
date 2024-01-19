b041d616dc1da191cbae90066d5ecf7e83ec45ba
alanhou <info@iscbj.com>
2024-01-19 13:32:35 +0800

    示例11-2：使用颜色填充形状

--- SwiftUI/Chapter11/ContentView.swift
+++ SwiftUI/Chapter11/ContentView.swift
@@ -9,23 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            ScrollView(.horizontal, showsIndicators: true) {
-                HStack {
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                    RoundedRectangle(cornerRadius: 25, style: .continuous)
-                        .frame(width: 100, height: 100)
-                    Circle()
-                        .frame(width: 100, height: 100)
-                    Ellipse()
-                        .frame(width: 100, height: 50)
-                    Capsule()
-                        .frame(width: 100, height: 50)
-                }.padding()
-            }
-            Spacer()
-        }
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.red)
+            .frame(width: 100, height: 100)
     }
 }
 
