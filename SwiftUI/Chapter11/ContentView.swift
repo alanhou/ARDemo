897688e80e675ed36af9d56889b9a9c68c242e97
alanhou <info@iscbj.com>
2024-01-29 13:58:34 +0800

    示例11-7：定义线性渐变

--- SwiftUI/Chapter11/ContentView.swift
+++ SwiftUI/Chapter11/ContentView.swift
@@ -8,22 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var setActive: Bool = true
+    let gradient = Gradient(colors: [Color.red, Color.green])
     
     var body: some View {
-        VStack {
-            Button(action: {
-                setActive.toggle()
-            }, label: {
-                Text(setActive ? "Active" : "Inactive")
-                    .font(.title)
-                    .foregroundColor(Color.white)
-                    .padding(.horizontal, 30)
-                    .padding(.vertical, 10)
-            })
-            .background(setActive ? Color.green : Color.red, in: Capsule())
-            Spacer()
-        }.padding()
+        RoundedRectangle(cornerRadius: 25)
+            .fill(.linearGradient(gradient, startPoint: .bottom, endPoint: .top))
+            .frame(width: 100, height: 100)
     }
 }
 
