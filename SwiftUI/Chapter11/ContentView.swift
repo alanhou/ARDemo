4a2db03c771b1bbd8a312da06a6d173526a299d1
alanhou <info@iscbj.com>
2024-01-31 11:04:47 +0800

    示例11-12：使用图片填充形状

--- SwiftUI/Chapter11/ContentView.swift
+++ SwiftUI/Chapter11/ContentView.swift
@@ -9,9 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        RoundedRectangle(cornerRadius: 25)
-            .foregroundStyle(.shadow(.drop(color: .black, radius: 3, x: 4, y: 4)))
-            .foregroundColor(.red)
+        Rectangle()
+            .fill(.image(Image(.pattern)))
             .frame(width: 100, height: 100)
     }
 }
