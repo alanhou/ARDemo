234611aa5a516c921d6dd44a52ea306f5dd19071
alanhou <info@iscbj.com>
2024-01-31 10:49:29 +0800

    示例11-11：对视图添加阴影

--- SwiftUI/Chapter11/ContentView.swift
+++ SwiftUI/Chapter11/ContentView.swift
@@ -8,11 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    let gradient = Gradient(colors: [Color.red, Color.white])
-    
     var body: some View {
         RoundedRectangle(cornerRadius: 25)
-            .fill(.conicGradient(gradient, center: .center, angle: .degrees(180)))
+            .foregroundStyle(.shadow(.drop(color: .black, radius: 3, x: 4, y: 4)))
+            .foregroundColor(.red)
             .frame(width: 100, height: 100)
     }
 }
