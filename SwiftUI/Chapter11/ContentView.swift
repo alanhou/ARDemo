0a7efb7419cff64b8eac672043af22a01c9aca5a
alanhou <info@iscbj.com>
2024-02-04 16:28:54 +0800

    示例11-16：创建复杂曲线

--- SwiftUI/Chapter11/ContentView.swift
+++ SwiftUI/Chapter11/ContentView.swift
@@ -10,9 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Path { path in
-            path.move(to: CGPoint(x: 100, y: 150))
-            path.addLine(to: CGPoint(x: 200, y: 150))
-            path.addEllipse(in: CGRect(x: 200, y: 140, width: 20, height: 20))
+            path.move(to: CGPoint(x: 50, y: 50))
+            path.addQuadCurve(to: CGPoint(x: 50, y: 200), control: CGPoint(x: 100, y: 125))
+            path.move(to: CGPoint(x: 250, y: 50))
+            path.addCurve(to: CGPoint(x: 250, y: 200), control1: CGPoint(x: 200, y: 125), control2: CGPoint(x: 300, y: 125))
         }.stroke(Color.blue, lineWidth: 5)
     }
 }
